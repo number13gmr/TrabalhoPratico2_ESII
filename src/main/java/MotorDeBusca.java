477c48c35b4e00eb8c00f12c2f009b16bd642832
MarioMendes9 <mariojmleite@hotmail.com>
2019-01-08 18:24:15 +0000

    Leitura apenas de ficheiros .txt esta com erro

--- src/main/java/MotorDeBusca.java
+++ src/main/java/MotorDeBusca.java
@@ -1,5 +1,7 @@
 
 import java.io.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class MotorDeBusca {
 
@@ -34,20 +36,22 @@ public class MotorDeBusca {
             filesName = new String[fList.length];
             //Cria um vetor apenas com o nome dos ficheiros
             for (int i = 0; i < fList.length; i++) {
-                if (fList[i].getName().endsWith(".txt")) {
+               // if (fList[i].getName().endsWith(".txt")) {
                     filesName[i] = fList[i].getName();
-                }
+               // }
             }
 
             //Adiciona o conteudo dos ficheiros a um vetor de string
             for (int i = 0; i < filesName.length; i++) {
                 File file = new File(directoryName + "/" + filesName[i]);
 
+
                 BufferedReader b = null;
 
                 try {
                     b = new BufferedReader(new FileReader(file));
                 } catch (FileNotFoundException e) {
+                    System.out.println(filesName[i]);
                     System.out.println("Problema ao abrir o ficheiro");
                 }
                 String aux = "";
@@ -67,5 +71,8 @@ public class MotorDeBusca {
             return this.fileContent;
         }
     }
+
+
+
 
 }
